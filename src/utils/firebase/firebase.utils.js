4a5753784865fc6babd25709278a2bc502da9071
James Mccarthy <mccarthyjames35@yahoo.com>
2024-06-18 00:04:11 -0700

    added siign in and fibease utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup ,GoogleAuthProvider} from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {
+  getFirestore, 
+  doc, 
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZv1fLRZTTfUzHdPdn7pDZX3yc9310Xns",
@@ -19,3 +25,34 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+
+    return userDocRef;
+  };
+}
+    // if user data does not exist
+  //create / set the document with the data from userAuth in my collection
+  
+  //if user data exists
+  // Return userDocRef
